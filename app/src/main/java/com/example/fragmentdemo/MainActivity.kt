494269525a43c727e4194dd1082ce576a3b8555c
Ivan Petr <ivan.kiel@gmail.com>
2022-03-07 13:19:39 +0800

    init2

--- app/src/main/java/com/example/fragmentdemo/MainActivity.kt
+++ app/src/main/java/com/example/fragmentdemo/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.fragmentdemo
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
 
 class MainActivity : AppCompatActivity() {
+    lateinit var fm: FragmentManager
+    lateinit var ft: FragmentTransaction
+    lateinit var fr1: Fragment
+    lateinit var fr2: Fragment
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        fm = supportFragmentManager
+        ft = fm.beginTransaction()
     }
 }
