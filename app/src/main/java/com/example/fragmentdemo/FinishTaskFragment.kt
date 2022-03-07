81a55029627f669696f0a20c5b068f4c90e52d2a
Ivan Petr <ivan.kiel@gmail.com>
2022-03-07 15:43:53 +0800

    fixed resources and classes

--- app/src/main/java/com/example/fragmentdemo/FinishTaskFragment.kt
+++ app/src/main/java/com/example/fragmentdemo/FinishTaskFragment.kt
@@ -7,14 +7,14 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 
-class CurrentTaskFragment: Fragment() {
+class FinishTaskFragment: Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_current_task, container, false)
-        view.setBackgroundColor(Color.BLUE)
+        view.setBackgroundColor(Color.YELLOW)
         return view
         //return super.onCreateView(inflater, container, savedInstanceState)
     }
